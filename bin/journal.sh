1d05dee0c2e707a074f4e0a3ad05c14ceb04724f
Cipherpig <Cipherpigg@gmail.com>
2026-01-20 05:33:50 +0000

    Add journal script

---
+++ bin/journal.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+JOURNAL_DIR="$HOME/journal"
+DATE="$(date +%F)"
+FILE="$JOURNAL_DIR/$DATE.md"
+
+mkdir -p "$JOURNAL_DIR"
+
+if [ ! -f "$FILE" ]; then
+  cat <<EOF > "$FILE"
+# Journal b
+ $DATE
+
+## Notes
+-
+EOF
+fi
+
+nano "$FILE"
+
+cd "$JOURNAL_DIR" || exit 1
+git add "$FILE"
+git commit -m "journal: $DATE"
+git push
+
